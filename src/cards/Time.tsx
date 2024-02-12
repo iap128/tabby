3ee24830c04180bd63547de14f30c1da4bcc9deb
Ryan Hunter <Ryan.Hunter@nike.com>
2024-02-12 13:55:06 -0800

    Updated clock logic

--- src/cards/Time.tsx
+++ src/cards/Time.tsx
@@ -1,15 +1,41 @@
 import { faClock } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Card, Statistic } from "antd";
+import { useEffect, useState } from "react";
 
 const Time = () => {
-    const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
+    const [time, setTime] = useState('');
+
+    const timeString = () => {
+        let returnString = '';
+
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+
+        if (hours > 12) {
+            returnString += ((hours - 12).toString());
+        }
+        else {
+            returnString += (hours.toString());
+        }
+
+        returnString +=(':');
+        returnString += (minutes.toString());
+        if (hours > 11) {
+            returnString += (' PM');
+        }
+
+        setTime(returnString);
+    }
+
+    useEffect(() => {
+        timeString();
+    }, []);
 
     return (
         <Card>
-            <Statistic title='Clock'  value={hours + ':' + minutes} prefix={<FontAwesomeIcon icon={faClock}/>} />
+            <Statistic title='Clock' value={time} prefix={<FontAwesomeIcon icon={faClock}/>} />
         </Card>
     );
 };
