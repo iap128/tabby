a90b84757316020b566e20dfd7770aca321dbeb5
iap128 <103337777+iap128@users.noreply.github.com>
2024-02-15 15:06:34 -0800

    Added 0 before minute is less than 10

--- src/cards/Time.tsx
+++ src/cards/Time.tsx
@@ -1,49 +1,57 @@
-import { faClock } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Card, Statistic } from "antd";
-import { useEffect, useState } from "react";
-import { ClockLoader } from "react-spinners";
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Card, Statistic } from 'antd';
+import { useEffect, useState } from 'react';
+import { ClockLoader } from 'react-spinners';
 
 const Time = () => {
-    const [time, setTime] = useState('');
+  const [time, setTime] = useState('');
 
-    const timeString = () => {
-        let returnString = '';
+  const timeString = () => {
+    let returnString = '';
 
-        const now = new Date();
-        const hours = now.getHours();
-        const minutes = now.getMinutes();
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
 
-        if (hours > 12) {
-            returnString += ((hours - 12).toString());
-        }
-        else {
-            returnString += (hours.toString());
-        }
+    if (hours > 12) {
+      returnString += (hours - 12).toString();
+    } else {
+      returnString += hours.toString();
+    }
 
-        returnString +=(':');
-        returnString += (minutes.toString());
-        if (hours > 11) {
-            returnString += (' PM');
-        }
-        else {
-            returnString += (' AM');
-        }
+    returnString += ':';
 
-        setTime(returnString);
-    };
+    if (minutes < 10) {
+      returnString += '0' + minutes.toString();
+    } else {
+      returnString += minutes.toString();
+    }
 
-    useEffect(() => {
-        const interval = setInterval(() => timeString(), 1000);
+    if (hours > 11) {
+      returnString += ' PM';
+    } else {
+      returnString += ' AM';
+    }
 
-        return () => clearInterval(interval);
-    }, []);
+    setTime(returnString);
+  };
 
-    return (
-        <Card>
-            {time === '' ? <ClockLoader /> : <Statistic title='Clock' value={time} prefix={<FontAwesomeIcon icon={faClock}/>} />}
-        </Card>
-    );
+  useEffect(() => {
+    const interval = setInterval(() => timeString(), 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Card>
+      {time === '' ? (
+        <ClockLoader />
+      ) : (
+        <Statistic title='Clock' value={time} prefix={<FontAwesomeIcon icon={faClock} />} />
+      )}
+    </Card>
+  );
 };
 
 export default Time;
