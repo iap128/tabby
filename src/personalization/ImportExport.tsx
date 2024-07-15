b8fe4f81fc6ae14badd6cbd272379e56551a3cfd
Ryan Hunter <Ryan.Hunter@nike.com>
2024-07-15 09:00:33 -0700

    Added file import

--- src/personalization/ImportExport.tsx
+++ src/personalization/ImportExport.tsx
@@ -1,34 +1,12 @@
 import { DownloadOutlined, UploadOutlined } from "@ant-design/icons";
-import { Upload, Button, UploadProps, Typography, Flex } from "antd";
+import { Button, Typography, Flex } from "antd";
 import { saveAs } from "file-saver";
 import { Config } from "../Config";
 import { setCookie } from "typescript-cookie";
+import Dropzone from "react-dropzone";
 
 const ImportExport = () => {
-    const props: UploadProps = {
-        name: 'file',
-        accept: '.json',
-        action: '/upload-do',
-        onChange(info) {
-            if (info.file.status === 'done') {
-                const parsedJSON = JSON.parse(info.file.response) as typeof Config;
-                console.log(info.file);
 
-                if (parsedJSON.apiKey) {
-                    setCookie('key', parsedJSON.apiKey);
-                }
-                if (parsedJSON.stationID) {
-                    setCookie('id', parsedJSON.stationID);
-                }
-                if (parsedJSON.weatherZip) {
-                    setCookie('zip', parsedJSON.weatherZip);
-                }
-                if (parsedJSON.links) {
-                    setCookie('links', JSON.stringify(parsedJSON.links));
-                }
-            }
-        },
-    };
 
     const importConfig = (file: File) => {
         const reader = new FileReader();
@@ -59,10 +37,17 @@ const ImportExport = () => {
     return (
         <Flex vertical gap={10}>
             <Typography.Title level={3}>Tabby Config</Typography.Title>
+
             <Button icon={<DownloadOutlined />} onClick={exportConfig}>Export Config</Button>
-            <Upload {...props}>
-                <Button icon={<UploadOutlined />}>Import Config</Button>
-            </Upload>
+
+            <Dropzone onDrop={files => importConfig(files[0])} maxFiles={1} accept={{"application/json": [".json"]}}>
+                {({getRootProps, getInputProps}) => (
+                    <div {...getRootProps({className: 'dropzone'})}>
+                        <input {...getInputProps()} />
+                        <Button block icon={<UploadOutlined />}>Import Config</Button>
+                    </div>
+                )}
+            </Dropzone>
         </Flex>
     );
 };
