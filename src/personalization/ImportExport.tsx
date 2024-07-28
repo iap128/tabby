5d0639f2a8bd729ceebf7ef2577debf08c46ae00
iap128 <103337777+iap128@users.noreply.github.com>
2024-07-27 17:42:49 -0700

    Swapped import/export icons

--- src/personalization/ImportExport.tsx
+++ src/personalization/ImportExport.tsx
@@ -1,55 +1,63 @@
-import { DownloadOutlined, UploadOutlined } from "@ant-design/icons";
-import { Button, Typography, Flex } from "antd";
-import { saveAs } from "file-saver";
-import { Config } from "../Config";
-import { setCookie } from "typescript-cookie";
-import Dropzone from "react-dropzone";
+import { DownloadOutlined, UploadOutlined } from '@ant-design/icons';
+import { Button, Typography, Flex } from 'antd';
+import { saveAs } from 'file-saver';
+import { Config } from '../Config';
+import { setCookie } from 'typescript-cookie';
+import Dropzone from 'react-dropzone';
 
 const ImportExport = () => {
-    const importConfig = (file: File) => {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            const parsedJSON = JSON.parse(e.target?.result as string) as typeof Config;
+  const importConfig = (file: File) => {
+    const reader = new FileReader();
+    reader.onload = e => {
+      const parsedJSON = JSON.parse(e.target?.result as string) as typeof Config;
 
-            if (parsedJSON.apiKey) {
-                setCookie('key', parsedJSON.apiKey);
-            }
-            if (parsedJSON.stationID) {
-                setCookie('id', parsedJSON.stationID);
-            }
-            if (parsedJSON.weatherZip) {
-                setCookie('zip', parsedJSON.weatherZip);
-            }
-            if (parsedJSON.links) {
-                setCookie('links', JSON.stringify(parsedJSON.links));
-            }
-        };
-        reader.readAsText(file);
-
-        window.location.reload();
+      if (parsedJSON.apiKey) {
+        setCookie('key', parsedJSON.apiKey);
+      }
+      if (parsedJSON.stationID) {
+        setCookie('id', parsedJSON.stationID);
+      }
+      if (parsedJSON.weatherZip) {
+        setCookie('zip', parsedJSON.weatherZip);
+      }
+      if (parsedJSON.links) {
+        setCookie('links', JSON.stringify(parsedJSON.links));
+      }
     };
+    reader.readAsText(file);
 
-    const exportConfig = () => {
-        const file = new Blob([JSON.stringify(Config)], {type: "text/plain;charset=utf-8"});
-        saveAs(file, 'tabby-config.json');
-    };
+    window.location.reload();
+  };
 
-    return (
-        <Flex vertical gap={10}>
-            <Typography.Title level={3}>Tabby Config</Typography.Title>
+  const exportConfig = () => {
+    const file = new Blob([JSON.stringify(Config)], { type: 'text/plain;charset=utf-8' });
+    saveAs(file, 'tabby-config.json');
+  };
 
-            <Button icon={<DownloadOutlined />} onClick={exportConfig}>Export Config</Button>
+  return (
+    <Flex vertical gap={10}>
+      <Typography.Title level={3}>Tabby Config</Typography.Title>
 
-            <Dropzone onDrop={files => importConfig(files[0])} maxFiles={1} accept={{"application/json": [".json"]}}>
-                {({getRootProps, getInputProps}) => (
-                    <div {...getRootProps({className: 'dropzone'})}>
-                        <input {...getInputProps()} />
-                        <Button block icon={<UploadOutlined />}>Import Config</Button>
-                    </div>
-                )}
-            </Dropzone>
-        </Flex>
-    );
+      <Button icon={<UploadOutlined />} onClick={exportConfig}>
+        Export Config
+      </Button>
+
+      <Dropzone
+        onDrop={files => importConfig(files[0])}
+        maxFiles={1}
+        accept={{ 'application/json': ['.json'] }}
+      >
+        {({ getRootProps, getInputProps }) => (
+          <div {...getRootProps({ className: 'dropzone' })}>
+            <input {...getInputProps()} />
+            <Button block icon={<DownloadOutlined />}>
+              Import Config
+            </Button>
+          </div>
+        )}
+      </Dropzone>
+    </Flex>
+  );
 };
 
 export default ImportExport;
