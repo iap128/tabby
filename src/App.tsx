4c0e77992d97f8942879d245438a77739b7f99c6
Ryan Hunter <Ryan.Hunter@nike.com>
2024-07-29 08:28:20 -0700

    Moved footer to bottom

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Time from './cards/Time';
 import Weather from './cards/Weather';
 import QuickLinks from './cards/QuickLinks';
 import CalendarCard from './cards/CalendarCard';
-import { Button, FloatButton } from 'antd';
+import { Button, FloatButton, Layout } from 'antd';
 import { useRef, useState } from 'react';
 import Settings from './personalization/Settings';
 import { SettingOutlined } from '@ant-design/icons';
@@ -31,29 +31,42 @@ function App() {
         height: '240px',
       }}
     >
-      <FloatButton ref={settingsRef} style={{ top: 5, right: 5 }} icon={<SettingOutlined />} type='primary' onClick={() => setOpen(true)} />
-      <Settings open={open} setOpen={setOpen}/>
-      <div style={{ display: 'flex', gap: '20px' }}>
-
+      <FloatButton
+        ref={settingsRef}
+        style={{ top: 5, right: 5 }}
+        icon={<SettingOutlined />}
+        type="primary"
+        onClick={() => setOpen(true)}
+      />
+      <Settings open={open} setOpen={setOpen} />
+      <div style={{ display: 'flex', gap: '20px', height: '85vh' }}>
         <div ref={weatherRef} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
           <Weather />
         </div>
 
-        <div ref={quickLinksRef} style={{ display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '33%' }}>
+        <div
+          ref={quickLinksRef}
+          style={{ display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '33%' }}
+        >
           <QuickLinks />
           <Time />
           <CalendarCard />
         </div>
       </div>
 
-      <AppTour closeAction={endTour} weatherRef={weatherRef} quickLinksRef={quickLinksRef} settingsRef={settingsRef} />
+      <AppTour
+        closeAction={endTour}
+        weatherRef={weatherRef}
+        quickLinksRef={quickLinksRef}
+        settingsRef={settingsRef}
+      />
 
-      <div style={{ position: 'absolute', bottom: 0, left: '45%', marginBottom: '10px'}}>
-      Made with ❤️ by{' '}
+      <Layout.Footer style={{ textAlign: 'center' }}>
+        Made with ❤️ by{' '}
         <Button type="dashed" href="https://n818pe.com">
           Ryan Hunter
         </Button>
-        </div>
+      </Layout.Footer>
     </div>
   );
 }
